c3fe3592acba416f1e9e7bc467b03399dc5be320
shander-debug <agrim310108@gmail.com>
2019-10-24 15:55:23 +0530

    hello

--- index.js
+++ index.js
@@ -26,19 +26,38 @@ client.on('message', message => {
  	})
  	
  	client.on('message', message => {
+ 	if(message.content === 'k!abhinew')
+ 	{
+ 		const attachment = new Discord.Attachment ('https://cdn.discordapp.com/attachments/636252281196773407/636521727010275358/IMG-20191023-WA0004.jpg')
+ 			message.channel.send(attachment)
+ 	}
+ 	})
+ 	
+ 	client.on('message', message => {
 	if(message.content === 'Hi')
 	{
 			       message.reply('Hello!')
  }
  })
- 
-  
+   
  client.on('message', message => {
 	if(message.content === 'am i right?')
 	{
 			       message.reply('yes u are,sir')
  }
  })
+ 
+client.on('message', message => {
+ if(message.content.startsWith('k!dm')
+ ){
+let content = message.content
+content = content.replace ('k!dm', '').subString(12)
+let user = message.mentions.roles.first().members.forEach((member) => {
+   member.send(content).catch()
+})
+message.delete().catch()
+}
+})
 
  client.on('message', message => {
  if(message.content.startsWith('k!say')
