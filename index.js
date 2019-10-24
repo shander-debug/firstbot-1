0c4506e1be40e787de6419c081695a6df9bae041
shander-debug <56389499+shander-debug@users.noreply.github.com>
2019-10-24 16:15:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Discord = require('discord.js');
  		 setInterval(function() {
  		 	let status = statuses[Math.floor(Math.random() * statuses.length)];
  		 	client.user.setActivity(status, {type: "PLAYING"}); 		 	
- 		 }, 5000)
+ 		 }, 4000)
  });
  
 client.on('message', message => {
